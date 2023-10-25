8a989c2fdfe64066e6239ebe660a9b19ad2fbb59
Vladimir Ilovaca <vladoilovaca@gmail.com>
2023-10-25 20:36:47 +0200

    Update

--- src/player.js
+++ src/player.js
@@ -5,6 +5,7 @@ class Player {
       this.height = 80; 
       this.x = 400; 
       this.y = 600;
+      this.vx = 0;
 
       this.element = document.createElement("div");
       this.element.style.position = "absolute"; 
@@ -19,5 +20,45 @@ class Player {
       this.element.style.top = `${this.y}px`;
   
       this.container.appendChild(this.element);
+
+      this.setListeners()
+    } 
+
+    move() {
+      this.x += this.vx;
+  
+      if (this.x <= 0) {
+        this.x = 0;
+      }
+  
+      if (this.x + this.width / 2 >= this.container.offsetWidth / 2) {
+        this.x = this.container.offsetWidth / 2 - this.width / 2;
+      }
+  
+      this.element.style.left = `${this.x}px`;
+
     }
-}
+
+    setListeners() {
+      window.addEventListener("keydown", (e) => {
+        switch (e.code) {
+          case "ArrowRight": 
+          this.vx = 10;
+          break;
+          case "ArrowLeft": 
+          this.vx = -10;
+        }
+      }); 
+
+      window.addEventListener("keyup", (e) => {
+        switch (e.code) {
+          case "ArrowRight":
+          case "ArrowLeft":
+            this.vx = 0;
+            break;
+        }
+      })
+    }
+
+} 
+
