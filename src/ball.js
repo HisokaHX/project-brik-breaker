26083f748f66d60d38f90c74f04c99e166513e87
Vladimir Ilovaca <vladoilovaca@gmail.com>
2023-10-27 19:15:14 +0200

    the ball is bouncing

--- src/ball.js
+++ src/ball.js
@@ -25,32 +25,30 @@ class Ball {
 
 
     move() {
+        
         this.x += this.vx;
-        this.y += this.vy;
-      
-        if (this.x <= 0) {
-          this.x = 0;
+        this.y += this.vy; 
+
+        if (this.x <= 0 || this.x + this.width >= this.container.offsetWidth) {
+            this.vx = -this.vx;
         }
-        if (this.x + this.width >= this.container.offsetWidth) {
-          this.x = this.container.offsetWidth - this.width;
+
+        if (this.y <= 0 || this.y + this.height >= this.container.offsetHeight) {
+            this.vy = -this.vy;
         }
-      
-        if (this.y <= 0) {
-          this.y = 0;
-        }
-        if (this.y + this.height >= this.container.offsetHeight) {
-          this.y = this.container.offsetHeight - this.height;
-        }
-      
+
         this.element.style.left = `${this.x}px`;
         this.element.style.top = `${this.y}px`;
-      }
+    } 
+
+    didCollied() {
+
+    }
 
     setListeners () {
         window.addEventListener("keydown", (e) => {
             switch (e.code) {
               case "Space":
-                console.log("esta entrando")
                 this.vy = -10;
                 this.vx = 10;
                 break;
